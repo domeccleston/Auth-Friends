4daa3a05f4bb4fe2f5b019f2633f56210e043cfe
Dom <domeccleston@gmail.com>
2019-10-15 20:20:07 +0100

    wip

--- friends/src/components/FriendForm.jsx
+++ friends/src/components/FriendForm.jsx
@@ -1,40 +1,32 @@
 import React, { useState, useRef } from "react";
 import { axiosWithAuth } from "./LoginForm";
-import uuid from 'uuid';
 
-const FriendForm = () => {
+const FriendForm = ({ newFriend, setNewFriend, handleChange }) => {
+
+  const newFriend, setNewFriend = useState()
 
   // set to initial number of friends and increment
   let testRef = useRef(7)
-  const [newFriend, setNewFriend] = useState({ id: "", name: "", age: "", email: "" })
 
-  const sendFriendData = event => {
-      event.preventDefault();
-      newFriend.id = useRef.current;
-      axiosWithAuth().post("http://localhost:5000/api/friends", newFriend)
-        .then(res => {
-            console.log(res)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-      setNewFriend({ ...newFriend, name: "", age: "", email: "" })
-  }
-
-  const handleChange = event => {
+  const handleEditChange = event => {
     setNewFriend({
-        ...newFriend,
-        [event.target.name]: event.target.value
-    })
+      ...newFriend,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const updateFriendData = event => {
+      event.preventDefault();
+      console.log(newFriend)
   }
 
   return (
     <div className="friend-form">
-      <h1>Add New Friend</h1>
-      <form onSubmit={sendFriendData}>
-        <input value={newFriend.name} onChange={handleChange} name="name" type="text" placeholder="Name" />
-        <input value={newFriend.age} onChange={handleChange} name="age" type="number" placeholder="Age" />
-        <input value={newFriend.email} onChange={handleChange} name="email" type="text" placeholder="Email" />
+      <h1>Edit Friend</h1>
+      <form onSubmit={updateFriendData}>
+        <input value={newFriend.name} onChange={handleEditChange} name="name" type="text" placeholder="Name" />
+        <input value={newFriend.age} onChange={handleEditChange} name="age" type="number" placeholder="Age" />
+        <input value={newFriend.email} onChange={handleEditChange} name="email" type="text" placeholder="Email" />
         <button type="submit">Submit</button>
       </form>
     </div>
