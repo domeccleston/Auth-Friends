66ac1cb8c039a068173a19c45e1ef2cadbc3b415
Dom <domeccleston@gmail.com>
2019-10-15 03:43:48 +0100

    endpoints

--- friends/src/components/FriendsList.jsx
+++ friends/src/components/FriendsList.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import { axiosWithAuth } from './LoginForm';
+
+
 
 const FriendsList = () => {
+    console.log(axiosWithAuth)
+    axiosWithAuth().get("http://localhost:5000/api/friends")
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.log(err)
+        })
     return (
         <div>List of friends will go here</div>
     );
